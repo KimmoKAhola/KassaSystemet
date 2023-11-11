d40c1d2cb58d1d89a7168023ec8a5aee80ceb745
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2023-11-11 09:06:22 +0100

    fix

--- KassaSystemet/Program.cs
+++ KassaSystemet/Program.cs
@@ -11,25 +11,11 @@ namespace KassaSystemet
     {
         static void Main(string[] args)
         {
-            GetValidProductPriceType();
             IFileManager fileManager = new FileManager(new FileManagerStrategy());
             MenuFactory menuFactory = new MenuFactory(fileManager);
             ModelFactory modelFactory = new ModelFactory(); //TODO remove?
             var myApp = new App(menuFactory);
             myApp.Run();
-        }
-        private static string GetValidProductPriceType()
-        {
-            while (true)
-            {
-                Console.Write("Enter a product price type (per kg/per unit): ");
-                string userInput = Console.ReadLine().ToLower();
-
-                if (userInput == "per kg" || userInput == "per unit")
-                    return userInput;
-                else
-                    Console.WriteLine("Invalid price type. Please enter 'per kg' or 'per unit'.");
-            }
         }
     }
 }
