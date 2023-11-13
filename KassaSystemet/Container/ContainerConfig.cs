af95a35e2a8f1e7e73018865456eddbfe0907e07
Kimmo Ahola <128542718+KimmoKAhola@users.noreply.github.com>
2023-11-13 11:02:58 +0100

    implementing autofac

---
+++ KassaSystemet/Container/ContainerConfig.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using KassaSystemet.Factories.MenuFactory;
+using KassaSystemet.File_IO;
+using KassaSystemet.Interfaces;
+using KassaSystemet.Menus.MenuPageHandlers;
+using KassaSystemet.Menus.MenuPages;
+using KassaSystemet.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KassaSystemet.Container
+{
+    public static class ContainerConfig
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<DefaultFileManager>().As<IFileManager>();
+            builder.RegisterType<UserInputHandler>().As<IUserInputHandler>();
+            builder.RegisterType<MenuFactory>().AsSelf();
+            builder.RegisterType<AppHandler>().SingleInstance();
+            builder.RegisterType<App>().As<IApplication>();
+            builder.RegisterType<AdminMenu>().SingleInstance();
+            builder.RegisterType<AdminMenuHandler>().As<IMenuHandler<AdminMenuEnum>>();
+            builder.RegisterType<CustomerMenu>().SingleInstance();
+            builder.RegisterType<CustomerMenuHandler>().As<IMenuHandler<CustomerMenuEnum>>();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ KassaSystemet/Interfaces/IApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KassaSystemet.Interfaces
+{
+    public interface IApplication
+    {
+        void StartApp();
+    }
+
+}
